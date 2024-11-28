e3f9a62013de23e3be9a0572cd6e2c74b92d2be1
anoopnamdev25 <namdevanoop8517@gmail.com>
2024-11-28 13:33:21 +0530

    first

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// const API_URL = 'http://localhost:5000/api/posts'; 
+const API_URL = 'http://13.236.118.222:5000/api/posts'; 
+
+export const fetchPosts = async () => axios.get(API_URL);
+export const fetchPostById = async (id) => axios.get(`${API_URL}/${id}`);
+export const createPost = async (postData) => axios.post(API_URL, postData);
+export const updatePost = async (id, updatedData) => axios.put(`${API_URL}/${id}`, updatedData);
+export const deletePost = async (id) => axios.delete(`${API_URL}/${id}`);
